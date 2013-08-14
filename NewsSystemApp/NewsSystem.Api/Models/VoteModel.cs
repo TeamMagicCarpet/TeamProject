6785ea4e6010288df1dc49f6eaa169cb525e4970
Antoveravip <petar@petarivanov.com>
2013-08-14 20:22:41 +0300

    Vote Model

---
+++ NewsSystemApp/NewsSystem.Api/Models/VoteModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsSystem.Api.Models
+{
+    public class VoteModel
+    {
+        public int VoteId { get; set; }
+
+        public virtual UserModel User { get; set; }
+
+        public virtual ArticleModel Article { get; set; }
+
+        public int Value { get; set; }
+    }
+}
