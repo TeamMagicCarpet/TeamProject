c81920313ad908d3ac8fc9945a520817e10c725e
vphilipov <vess.ph@gmail.com>
2013-08-14 14:19:30 +0300

    Added data layer context.

---
+++ NewsSystemApp/NewsSystem.Data/NewsSystemContext.cs
@@ -0,0 +1,35 @@
+﻿using NewsSystem.Models;
+using System.Data.Entity;
+
+namespace NewsSystem.Data
+{
+    public class NewsSystemContext : DbContext
+    {
+        public NewsSystemContext()
+            : base("Data Source=.;Initial Catalog=NewsSystemDB;Integrated Security=True")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.UserName)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Password)
+                .IsRequired()
+                .IsFixedLength()
+                .HasMaxLength(40);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
