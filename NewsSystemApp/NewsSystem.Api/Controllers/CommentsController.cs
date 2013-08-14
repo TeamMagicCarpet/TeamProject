7d5e72e0a09256b51fbd6379bbe45d4b497d7c19
vphilipov <vess.ph@gmail.com>
2013-08-14 22:20:13 +0300

    Added Post to CommentsController.
    Updated DbDependencyResolver.
    Updated CommentModel.

--- NewsSystemApp/NewsSystem.Api/Controllers/CommentsController.cs
+++ NewsSystemApp/NewsSystem.Api/Controllers/CommentsController.cs
@@ -13,28 +13,60 @@ namespace NewsSystem.Api.Controllers
     public class CommentsController : ApiController
     {
         private readonly IRepository<Comment> commentRepository;
+        private readonly IRepository<Article> articleRepository;
+        private readonly IRepository<User> userRepository;
 
-        public CommentsController(IRepository<Comment> commentRepository)
+        public CommentsController(IRepository<Comment> commentRepository,
+            IRepository<Article> articleRepository, IRepository<User> userRepository)
         {
             this.commentRepository = commentRepository;
+            this.articleRepository = articleRepository;
+            this.userRepository = userRepository;
         }
 
         [HttpGet]
-        public IEnumerable<CommentDetailsModel> GetAll()
+        public IEnumerable<CommentDetailsModel> GetArticleComments(int articleId)
         {
-            var commentEntities = this.commentRepository.All();
+            var commentEntities = this.commentRepository.All().Where(c => c.Article.ArticleId == articleId);
             var commentModels =
                 from commentEntity in commentEntities
                 select new CommentDetailsModel()
                 {
+                    Content = commentEntity.Content,
+                    Article = commentEntity.Article,
                     User = commentEntity.User,
-
-                    //UserId = commentEntity.UserId,
-                    //FirstName = commentEntity.FirstName,
-                    //LastName = commentEntity.LastName,
-                    //UserName = commentEntity.User,
+                    Answers = commentEntity.Answers
                 };
             return commentModels.ToList();
+        }
+
+        [HttpPost]
+        public HttpResponseMessage PostComment(CommentModel comment, int articleId, int authorId)
+        {
+            Article article = articleRepository.Get(articleId);
+            User author = userRepository.Get(authorId);
+            var entityToAdd = new Comment()
+            {
+                Content = comment.Content,
+                Article = article,
+                User = author
+            };
+
+            var createEntity = this.commentRepository.Add(entityToAdd);
+
+            var createdModel = new CommentModel()
+            {
+                CommentId = createEntity.CommentId,
+                Content = createEntity.Content,
+                Article = createEntity.Article,
+                User = createEntity.User
+            };
+
+            var response = Request.CreateResponse<CommentModel>(HttpStatusCode.Created, createdModel);
+            var resourceLink = Url.Link("DefaultApi", new { CommentId = createdModel.CommentId });
+            response.Headers.Location = new Uri(resourceLink);
+
+            return response;
         }
     }
 }
