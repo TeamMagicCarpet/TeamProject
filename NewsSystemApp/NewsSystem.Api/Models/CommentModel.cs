35c16fc999ed1101a5ee0724bde718a9516f5caa
vphilipov <vess.ph@gmail.com>
2013-08-14 20:06:14 +0300

    Added Comment and Image models in Api. Fixed usings for Article and User models in Api.

---
+++ NewsSystemApp/NewsSystem.Api/Models/CommentModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace NewsSystem.Api.Models
+{
+    public class CommentModel
+    {
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public UserModel User { get; set; }
+        public ArticleModel Article { get; set; }
+    }
+
+    public class CommentDetailsModel : CommentModel
+    {
+        public virtual ICollection<CommentDetailsModel> Answers { get; set; }
+    }
+}
