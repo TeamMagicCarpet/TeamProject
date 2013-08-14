0d280908673a75edcdb34134cb40c868f8ea6e6f
Antoveravip <petar@petarivanov.com>
2013-08-14 13:33:02 +0300

    Vote Model

--- NewsSystemApp/NewsSystem.Models/Vote.cs
+++ NewsSystemApp/NewsSystem.Models/Vote.cs
@@ -8,5 +8,27 @@ namespace NewsSystem.Models
 {
     public class Vote
     {
+        public int VoteId { get; set; }
+
+        private int voteValue;
+        public int Value
+        {
+            get
+            {
+                return this.voteValue;
+            }
+            set
+            {
+                if (0 > value && value > 10)
+                {
+                    throw new ArgumentOutOfRangeException("The voteValue value must be in range [0 - 10]!");
+                }
+
+                this.voteValue = value;
+            }
+        }
+
+        public virtual User User { get; set; }
+        public virtual Article Article { get; set; }
     }
 }
